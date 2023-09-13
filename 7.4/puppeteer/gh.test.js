4cb359207ec70d1e0336412647dcf6018fdb1205
Anastasiya <makeeva.anastasya89@gmail.com>
2023-09-14 01:07:06 +0300

    added tests for check headers

--- 7.4/puppeteer/gh.test.js
+++ 7.4/puppeteer/gh.test.js
@@ -2,7 +2,6 @@ let page;
 
 beforeEach(async () => {
   page = await browser.newPage();
-  await page.goto("https://github.com/team");
 });
 
 afterEach(() => {
@@ -10,25 +9,56 @@ afterEach(() => {
 });
 
 describe("Github page tests", () => {
+  beforeEach(async () => {
+    await page.goto("https://github.com/team");
+  });
+
   test("The h1 header content'", async () => {
     const firstLink = await page.$("header div div a");
     await firstLink.click();
-    await page.waitForSelector('h1');
+    await page.waitForSelector("h1");
     const title2 = await page.title();
-    expect(title2).toEqual('GitHub: Where the world builds software · GitHub');
-  });
+    expect(title2).toEqual(
+      "GitHub for teams · Build like the best teams on the planet · GitHub"
+    );
+  }, 2000);
 
   test("The first link attribute", async () => {
-    const actual = await page.$eval("a", link => link.getAttribute('href') );
+    const actual = await page.$eval("a", (link) => link.getAttribute("href"));
     expect(actual).toEqual("#start-of-content");
-  });
+  }, 1000);
 
   test("The page contains Sign in button", async () => {
     const btnSelector = ".btn-large-mktg.btn-mktg";
     await page.waitForSelector(btnSelector, {
       visible: true,
     });
-    const actual = await page.$eval(btnSelector, link => link.textContent);
-    expect(actual).toContain("Sign up for free")
-  });
+    const actual = await page.$eval(btnSelector, (link) => link.textContent);
+    expect(actual).toContain("Get started with Team");
+  }, 2000);
 });
+
+test("Check header on the page 'Marketplace'", async () => {
+  await page.goto("https://github.com/marketplace");
+  await page.waitForSelector("h1");
+  const header = await page.title();
+  expect(header).toEqual(
+    "GitHub Marketplace · to improve your workflow · GitHub"
+  );
+}, 2000);
+
+test("Check header on the page 'About'", async () => {
+  await page.goto("https://github.com/about");
+  await page.waitForSelector("h1");
+  const header = await page.title();
+  expect(header).toEqual("About · GitHub");
+}, 2000);
+
+test("Check header on the page 'Enterprise'", async () => {
+  await page.goto("https://github.com/enterprise");
+  await page.waitForSelector("h1");
+  const header = await page.title();
+  expect(header).toEqual(
+    "Enterprise · A smarter way to work together · GitHub"
+  );
+}, 2000);
